4e92971b06889f6b229d7b7b79379c4cc3d1e196
Pryscilla123 <pryscillaromao@hotmail.com>
2025-06-28 18:55:37 -0300

    Adicionando Controller de Fornecedores e configurando a Program.cs

--- src/DevIO.Api/Program.cs
+++ src/DevIO.Api/Program.cs
@@ -1,8 +1,22 @@
+using DevIO.Api.Configurations;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<MeuDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.ResolveDependencies();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,6 +26,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
