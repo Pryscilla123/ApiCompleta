4060babf0eb20039b9166128519f96b3041c67ab
Pryscilla123 <pryscillaromao@hotmail.com>
2025-07-02 21:59:29 -0300

    Criando rota de adicionar produto.

--- src/DevIO.Api/Controllers/ProdutosController.cs
+++ src/DevIO.Api/Controllers/ProdutosController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using DevIO.Api.ViewModels;
 using DevIO.Business.Intefaces;
+using DevIO.Business.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevIO.Api.Controllers
@@ -23,9 +24,80 @@ namespace DevIO.Api.Controllers
         }
 
         [HttpGet]
-        public async Task<IEnumerable<ProdutoViewModel>> Get()
+        public async Task<ActionResult<IEnumerable<ProdutoViewModel>>> Get()
         {
-            return _mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterTodos());
+            return CustomResponse(_mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterProdutosFornecedores()));
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> Get(Guid id)
+        {
+            var produtoViewModel = _mapper.Map<ProdutoViewModel>(await _produtoRepository.ObterProdutosPorFornecedor(id));
+
+            if(produtoViewModel == null) return NotFound();
+
+            return CustomResponse(produtoViewModel);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProdutoViewModel>> Post([FromBody] ProdutoViewModel produtoViewModel)
+        {
+            if(!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var imagemNome = Guid.NewGuid() + "_" + produtoViewModel.Imagem;
+
+            if(!UploadArquivo(produtoViewModel.ImagemUpload, imagemNome))
+            {
+                return CustomResponse(produtoViewModel);
+            }
+
+            produtoViewModel.Imagem = imagemNome;
+
+            await _produtoService.Adicionar(_mapper.Map<Produto>(produtoViewModel));
+
+            return CustomResponse(produtoViewModel);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> Delete(Guid id)
+        {
+            var produto = await _produtoRepository.ObterPorId(id);
+
+            if (produto == null) return NotFound();
+
+            await _produtoService.Remover(id);
+
+            return CustomResponse(produto);
+        }
+
+        private bool UploadArquivo(string arquivo, string imgNome)
+        {
+            var imageDataByteArray = Convert.FromBase64String(arquivo);
+
+            if (string.IsNullOrEmpty(arquivo))
+            {
+                NotificarErro("Forneça uma imagem para este produto!");
+                return false;
+            }
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens", imgNome);
+
+            if (System.IO.File.Exists(filePath))
+            {
+                NotificarErro("Já existe um arquivo com este nome!");
+                return false;
+            }
+
+            try
+            {
+                System.IO.File.WriteAllBytes(filePath, imageDataByteArray);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                NotificarErro($"Não foi possível salvar a imagem: {ex.Message}");
+                return false;
+            }
         }
     }
 }
